4b5c27daf6a57a8af3eeeb57593ce01f7c7d7fc3
Shashwat-19 <shashwat1956@gmail.com>
2025-11-27 13:30:52 +0530

    fix(server): switch to IPv4 host and new port to fix ENOTSUP listen error on macOS.

--- server/index.ts
+++ server/index.ts
@@ -70,16 +70,15 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = 5055;
 
-  const isWindows = process.platform === 'win32';
-  const host = isWindows ? 'localhost' : '0.0.0.0';
+  const host = '127.0.0.1';
 
   server.listen({
     port,
     host,
-    reusePort: !isWindows,
+    //reusePort: !isWindows,
   }, () => {
-    log(`serving on port ${port}`);
+    log(`serving on http://${host}:${port}`);
   });
 })();
